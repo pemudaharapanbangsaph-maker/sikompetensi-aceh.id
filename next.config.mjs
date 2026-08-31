b6c5b967fb55991cb8397a5366e0619a1472f0e0
pemudaharapanbangsaph-maker <pemudaharapanbangsa.ph@gmail.com>
2026-08-31 22:58:35 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,19 @@
-import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'path'
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
   webpack: (config) => {
-    config.resolve.alias['@'] = resolve(__dirname, 'src')
-    return config
+    config.resolve.alias["@"] = resolve(__dirname, "src");
+    return config;
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
