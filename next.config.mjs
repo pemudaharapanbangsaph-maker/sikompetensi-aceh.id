7a99f2a1aed366a3f07f7a5de7d292f8a52b61b9
pemudaharapanbangsaph-maker <pemudaharapanbangsa.ph@gmail.com>
2026-09-01 00:41:27 +0700

    Remove standalone output option from next config

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,6 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 const nextConfig = {
-  output: 'standalone',
   typescript: { ignoreBuildErrors: true },
   reactStrictMode: false,
   webpack: (config) => {
