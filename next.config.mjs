f620a0096a7ffe758bbc4271d4566617b9385a04
pemudaharapanbangsaph-maker <pemudaharapanbangsa.ph@gmail.com>
2026-09-01 01:18:50 +0700

    Fix formatting in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ import { dirname, resolve } from 'path'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   reactStrictMode: false,
