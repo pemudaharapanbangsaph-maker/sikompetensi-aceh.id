999fa2b113d2371ae80768845f1f7c108748397b
pemudaharapanbangsaph-maker <pemudaharapanbangsa.ph@gmail.com>
2026-08-31 23:06:35 +0700

    Refactor next.config.mjs for consistency and style

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,20 @@
-import { fileURLToPath } from "node:url";
-import { dirname, resolve } from "node:path";
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-/** @type {import("next").NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   typescript: {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
   webpack: (config) => {
-    config.resolve.alias["@"] = resolve(__dirname, "src");
-    return config;
+    config.resolve.alias['@'] = resolve(__dirname, 'src')
+    return config
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
